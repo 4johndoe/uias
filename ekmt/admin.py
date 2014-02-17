3b5846c505251500d51cf5428f20af58971ef582
4johndoe <bogdandemchenko@gmail.com>
2014-02-17 11:01:38 +0200

    add_ekmt_againe

--- ekmt/admin.py
+++ ekmt/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from ekmt.models import LicenseBook, Firm
 
-class LicBookInline(admin.StackedInline):
-    model = LicenseBook
-
-class FirmAdmin(admin.ModelAdmin):
-    list_display = ('name', 'edrpo')
-
-    search_fields = ['name', 'edrpo', 'number']
-
-    fieldsets = [
-        (None, {'fields': ['name']}),
-        (None, {'fields': ['edrpo']}),
-    ]
-
-    inlines = [LicBookInline]
-
-admin.site.register(Firm, FirmAdmin)
+# Register your models here.
--- ekmt/models.py
+++ ekmt/models.py
@@ -1,32 +1,3 @@
 from django.db import models
 
-class Firm(models.Model):
-    name = models.CharField(max_length=100)
-    edrpo = models.IntegerField(default=0, max_length=8)
-    amount = models.IntegerField(default=0, max_length=3)
-
-    def __unicode__(self):
-        return self.name
-
-class LicenseBook(models.Model):
-    firm = models.ForeignKey(Firm)
-    number = models.IntegerField(default=0)
-
-    def __unicode__(self):
-        return self.number
-
-class Carriage(models.Model):
-    _license = models.ForeignKey(LicenseBook)
-    departure_date = models.DateField('departure_date')
-    arrival_date = models.DateField('arrival_date')
-    loading_point = models.CharField(max_length=20)
-    unloading_point = models.CharField(max_length=20)
-    loading_country = models.CharField(max_length=2)
-    unloading_country = models.CharField(max_length=2)
-    truck_nom = models.CharField(max_length=8)
-    trailer_nom = models.CharField(max_length=8)
-    country = models.CharField(max_length=2)
-    weight = models.IntegerField(default=0)
-    speedometr_before = models.IntegerField(default=0)
-    speedometr_after = models.IntegerField(default=0)
-    etc = models.IntegerField(default=0)
+# Create your models here.
