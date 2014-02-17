e7976a51ceca684bfe91b22afa621f8a8772e9a5
4johndoe <bogdandemchenko@gmail.com>
2014-02-17 15:53:10 +0200

    try

--- ekmt/admin.py
+++ ekmt/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from ekmt.models import License, Carriage
 
-# Register your models here.
+class CarriageInline(admin.TabularInline):
+    model = Carriage
+    extra = 1
+
+class EkmtAdmin(admin.ModelAdmin):
+    list_display = ('number', 'firm', 'kod')
+    list_filter = ['firm']
+    search_fields = ['kod']
+    inlines = [CarriageInline]
+
+
+admin.site.register(License, EkmtAdmin)
--- ekmt/models.py
+++ ekmt/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class License(models.Model):
+    number = models.IntegerField(max_length=6)
+    firm = models.CharField(max_length=20)
+    kod = models.IntegerField(max_length=10)
+
+    def __unicode__(self):
+        # return self.number.__str__() + ' ' + self.firm
+        return self.number.__str__()
+
+
+class Carriage(models.Model):
+    lic = models.ForeignKey(License)
+    departure_date = models.DateField()
+    arrival_date = models.DateField()
+    loading_point = models.CharField(max_length=20)
+    unloading_point = models.CharField(max_length=20)
+    loading_country = models.CharField(max_length=2)
+    unloading_country = models.CharField(max_length=2)
+    truck_nom = models.CharField(max_length=8)
+    trailer_nom = models.CharField(max_length=8)
+    country_of_reg = models.CharField(max_length=2)
+    weight = models.IntegerField(default=0)
+    speedometr_before = models.IntegerField()
+    speedometr_after = models.IntegerField()
+    etc = models.IntegerField()
+
+    def __unicode__(self):
+        pass
